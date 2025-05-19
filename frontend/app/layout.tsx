e62d99b739c328f29466e7fdebd5a58129f1e1a8
dina juliarti <dina_juliarti@teknokrat.ac.id>
2025-05-19 21:42:39 +0700

    dina: menambahkan font poppins dan meta data

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,20 @@ import { Poppins,  } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Permata Roti",
+  description: "Web katalog produk roti dari Permata Roti",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -12,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="id">
       <body
+        className={`${poppins.className}`}
       >
         <Navbar />
         {children}
