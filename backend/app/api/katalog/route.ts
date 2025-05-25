061aa19cc500e46ad47237cb2d11d03f27e92f05
shofiyah <shofiyahnrjnnh@gmail.com>
2025-05-26 00:21:50 +0700

    shofiyah: menambahkan post dan get method katalog all

---
+++ backend/app/api/katalog/route.ts
@@ -0,0 +1,78 @@
+import { metadata } from "@/app/layout";
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (request: NextRequest) => {
+  try {
+    const body = await request.json();
+    const { name, description, price, imageUrl } = body;
+
+    if (!name || !price) {
+      return NextResponse.json(
+        { message: "Name and price are required." },
+        { status: 400 }
+      );
+    }
+
+    const newItem = await db.katalog.create({
+      data: {
+        name,
+        description,
+        price,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        metadata: {
+          error: 0,
+          message: "Item berhasil ditambahkan",
+          status: 201,
+        },
+        data_view: newItem,
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Failed to create item" },
+      { status: 500 }
+    );
+  }
+};
+
+export const GET = async () => {
+  try {
+    const katalog = await db.katalog.findMany();
+    return NextResponse.json(
+      {
+        metadata: {
+          error: 0,
+          message: "Semua katalog berhasil diambil",
+          status: 201,
+        },
+        data_view: katalog,
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (err: unknown) {
+    console.log(err);
+    return NextResponse.json(
+      {
+        metadata: {
+          error: 1,
+          message: `${err}`,
+          status: 500,
+        },
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
