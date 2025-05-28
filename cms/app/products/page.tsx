d755df3a757447218a8a2875e094fb1dee24a63b
nabila <nabilapc06@gmail.com>
2025-05-28 14:34:05 +0700

    nabila: add products page

---
+++ cms/app/products/page.tsx
@@ -0,0 +1,12 @@
+import ProductsTable from "@/components/UI/table/ProductsTable";
+import React from "react";
+
+function page() {
+  return (
+    <div className="p-10">
+      <ProductsTable />
+    </div>
+  );
+}
+
+export default page;
