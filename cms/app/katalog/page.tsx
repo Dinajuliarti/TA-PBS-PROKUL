0214d817a8a234db691ef99e54392d3d02f5255c
nabila <nabilapc06@gmail.com>
2025-05-28 14:35:08 +0700

    nabila: add katalog page

---
+++ cms/app/katalog/page.tsx
@@ -0,0 +1,11 @@
+import BreadProductCard from "@/components/UI/card/BreadProductsCard";
+
+function page() {
+  return (
+      <div className="">
+          <BreadProductCard />
+    </div>
+  );
+}
+
+export default page;
