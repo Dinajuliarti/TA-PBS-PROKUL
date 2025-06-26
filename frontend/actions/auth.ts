cb132c0af8c0c11264d9d9e8aa1ca4462555dcdf
shofiyah <shofiyahnrjnnh@gmail.com>
2025-06-26 16:03:55 +0700

    Update auth.ts

--- frontend/actions/auth.ts
+++ frontend/actions/auth.ts
@@ -8,6 +8,7 @@ type LoginState = {
   success: boolean;
   error?: string;
   user?: any;
+  token?: string; // Tambahkan field token
 };
 
 export async function loginActionWithState(
@@ -19,53 +20,31 @@ export async function loginActionWithState(
 
   try {
     const response = await api.post("/api/auth/login", { email, password });
+    const { user, token } = response.data;
 
     // Simpan token di cookie untuk SSR
     (
       await // Simpan token di cookie untuk SSR
       cookies()
-    ).set("token", response.data.token, {
+    ).set("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      maxAge: 60 * 60,
+      maxAge: 60 * 60 * 24 * 7, // 1 minggu
       path: "/",
     });
 
-
-    return { success: true, user: response.data.user };
+    return {
+      success: true,
+      user,
+      token,
+    };
   } catch (error: any) {
-    return { success: false, error: error?.message || "Login failed" };
+    return {
+      success: false,
+      error: error.response?.data?.error || "Login Gagal",
+    };
   }
 }
-
-// export async function loginAction(formData: FormData) {
-//   const email = formData.get("email") as string;
-//   const password = formData.get("password") as string;
-
-//   try {
-//     const response = await api.post("/api/auth/login", { email, password });
-
-//     // Simpan token di localStorage (client-side)
-//     if (typeof window !== "undefined") {
-//       localStorage.setItem("token", response.data.token);
-//     }
-
-//     // Set cookie untuk server-side access
-//     (
-//       await // Set cookie untuk server-side access
-//       cookies()
-//     ).set("token", response.data.token, {
-//       httpOnly: true,
-//       secure: process.env.NODE_ENV === "production",
-//       maxAge: 60 * 60, // 1 jam
-//       path: "/",
-//     });
-
-//     return { success: true, user: response.data.user };
-//   } catch (error) {
-//     return { success: false, error: "Login failed" };
-//   }
-// }
 
 export async function registerAction(formData: FormData) {
   const name = formData.get("name") as string;
