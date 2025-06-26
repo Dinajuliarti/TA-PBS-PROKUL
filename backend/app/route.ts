921de51a199c8c1f382162d7c27c1d8999d790f7
dina <dina_juliarti@teknokrat.ac.id>
2025-06-26 13:25:59 +0700

    Update route.ts

--- backend/app/route.ts
+++ backend/app/route.ts
@@ -1,3 +1,18 @@
-export async function GET(req:NextRequest) {
-  return NextResponse.json({API PERMATA ROTI})
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  return NextResponse.json(
+    { "API PERMATA ROTI": "v1.0.0", status: "OK" },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Authorization, X-CSRF-Token",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Expose-Headers": "Set-Cookie",
+      },
+    }
+  );
 }
