a259e5bfd434ddda71b8ac75441a1ea70318bd6d
shofiyah <shofiyahnrjnnh@gmail.com>
2025-06-26 16:50:04 +0700

    Update ProductsContent.tsx

--- frontend/components/content/ProductsContent.tsx
+++ frontend/components/content/ProductsContent.tsx
@@ -124,7 +124,7 @@ export default function ProductsContent({
   // Tampilkan loading spinner di tengah layar
   if (loading) {
     return (
-      <div className="fixed w-100 inset-0 flex items-center justify-center bg-white bg-opacity-80 z-50">
+      <div className="fixed ml-64 inset-0 flex items-center justify-center bg-white bg-opacity-80 z-50">
         <div className="flex flex-col items-center">
           <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-amber-500"></div>
           <p className="mt-4 text-lg font-medium">Memuat produk...</p>
