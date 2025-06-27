3d90f7eafc6e923a0a8246cd1066473281285b84
nabila <nabilapc06@gmail.com>
2025-06-27 15:01:44 +0700

    Create page.tsx

---
+++ cms/app/(admin)/users/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserReportTable from "../dashboard/components/UsersReportTable";
+
+function page() {
+  return (
+    <div className="p-10">
+      <UserReportTable /> 
+    </div>
+  )
+}
+
+export default page;
